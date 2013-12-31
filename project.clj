5a36c90a74083da28b844a2f613961b8abe7c77d
mynomoto <mynomoto@gmail.com>
2013-12-31 11:08:26 -0200

    Update java.jdbc to 0.3.2

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/java.jdbc "0.3.0"]]
+                 [org.clojure/java.jdbc "0.3.2"]]
   :profiles {:dev {:dependencies [[com.h2database/h2 "1.3.174"]
                                   [org.hsqldb/hsqldb "2.3.1"]
                                   [org.xerial/sqlite-jdbc "3.7.2"]
