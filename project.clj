a069928e98f3080fc591dc69fd86ac4b2e88035c
mynomoto <mynomoto@gmail.com>
2014-02-05 19:29:30 -0200

    Add lein-test-refresh config

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/java.jdbc "0.3.3"]]
+  :test-refresh  {:notify-command  ["lein-test"]}
   :profiles {:dev {:dependencies [[com.h2database/h2 "1.3.174"]]}})
