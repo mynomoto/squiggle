5720c824d3a78022b4f1a90acbbd99d4e72f1d79
mynomoto <mynomoto@gmail.com>
2014-02-05 14:38:44 -0200

    Update c.j.j

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/java.jdbc "0.3.2"]]
+                 [org.clojure/java.jdbc "0.3.3"]]
   :profiles {:dev {:dependencies [[com.h2database/h2 "1.3.174"]]}})
